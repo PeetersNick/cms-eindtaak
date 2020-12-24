4d84bab3a82611a788bbdbd4ac8a2162248b235e
Nick Peeters <tisnickske@gmail.com>
2020-12-24 12:29:13 +0100

    create movies page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react"
-import { Link, useStaticQuery, graphql} from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
-import { Wrapper, Image, Movie, BottomEdgeDown, BottomEdgeUp} from "./pageStyles/pageStyles"
-import {COLORS} from '../constants'
+import {
+  Wrapper,
+  Image,
+  Movie,
+  BottomEdgeDown,
+  BottomEdgeUp,
+} from "./pageStyles/pageStyles"
+import { COLORS } from "../constants"
 
 const IndexPage = () => {
   const {
@@ -16,40 +22,42 @@ const IndexPage = () => {
           homePageBanner,
           homePageFeaturedMovies,
           homePageDescription,
-        }
-      }
-    }
-  } =  useStaticQuery(graphql`
-  query {
-  wpcontent {
-    page(id: "home", idType: URI) {
-      homePageMeta {
-        homePageTitle
-        homePage
-        homePageDescription
-        homePageBanner {
-          altText
-          sourceUrl
-          imageFile{
-            childImageSharp{
-              fluid(quality: 100){
-                ...GatsbyImageSharpFluid_withWebp
+        },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "home", idType: URI) {
+          homePageMeta {
+            homePageTitle
+            homePage
+            homePageDescription
+            homePageBanner {
+              altText
+              sourceUrl
+              imageFile {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
               }
             }
-          }
-        }
-        homePageFeaturedMovies {
-          ... on WPGraphql_Movie {
-            id
-            movie {
-              title
-              poster {
-                altText
-                sourceUrl
-                imageFile{
-                  childImageSharp{
-                    fluid(quality: 100){
-                      ...GatsbyImageSharpFluid_withWebp
+            homePageFeaturedMovies {
+              ... on WPGraphql_Movie {
+                id
+                movie {
+                  title
+                  poster {
+                    altText
+                    sourceUrl
+                    imageFile {
+                      childImageSharp {
+                        fluid(quality: 100) {
+                          ...GatsbyImageSharpFluid_withWebp
+                        }
+                      }
                     }
                   }
                 }
@@ -59,41 +67,45 @@ const IndexPage = () => {
         }
       }
     }
-  }
-}
   `)
 
-  return   (
-  <Layout>
-    <SEO title="Home" />
-    <Wrapper>
-      <div className="banner">
-        <Image fluid={homePageBanner.imageFile.childImageSharp.fluid} altText={homePageBanner.altText}/>
-        <div className="inner-div">
-          <p className="header-title">{homePageTitle}</p>
-          <p className="header-description">{homePage}</p>
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Wrapper>
+        <div className="banner">
+          <Image
+            fluid={homePageBanner.imageFile.childImageSharp.fluid}
+            altText={homePageBanner.altText}
+          />
+          <div className="inner-div">
+            <p className="header-title">{homePageTitle}</p>
+            <p className="header-description">{homePage}</p>
+          </div>
+          <BottomEdgeDown color={COLORS.BLACK} />
         </div>
-        <BottomEdgeDown color={COLORS.BLACK}/>
-      </div>
-      <div className="description">
-      <p>{homePageDescription}</p>
-      <BottomEdgeUp color={COLORS.PRIMARY}/>
-      </div>
-      <div className="movies">
-        <h2>Featured Movies</h2>
-        <div className="movie-items">
-          {homePageFeaturedMovies.map(({movie, title}) => (
-            <Movie to={`/${title}`}>
-              <Image fluid={movie.poster.imageFile.childImageSharp.fluid} altText={movie.poster.altText}/>
-              <div className="movie-info">
-              <p>{movie.title}</p>
-              </div>
-            </Movie>
-          ))}
+        <div className="description">
+          <p>{homePageDescription}</p>
+          <BottomEdgeUp color={COLORS.PRIMARY} />
         </div>
-      </div>
-    </Wrapper>
-  </Layout>
+        <div className="movies">
+          <h2>Featured Movies</h2>
+          <div className="movie-items">
+            {homePageFeaturedMovies.map(({ movie, title }) => (
+              <Movie to={`/${title}`}>
+                <Image
+                  fluid={movie.poster.imageFile.childImageSharp.fluid}
+                  altText={movie.poster.altText}
+                />
+                <div className="movie-info">
+                  <p>{movie.title}</p>
+                </div>
+              </Movie>
+            ))}
+          </div>
+        </div>
+      </Wrapper>
+    </Layout>
   )
 }
 
---
+++ src/pages/movies.js
@@ -0,0 +1,104 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import {
+  Wrapper,
+  Image,
+  BottomEdgeDown,
+  BottomEdgeUp,
+  Movie,
+} from "./pageStyles/pageStyles"
+import { COLORS } from "../constants"
+
+const MoviesPage = () => {
+  const {
+    wpcontent: {
+      page: {
+        moviesMeta: { moviesPageBanner, moviesPageDescription },
+      },
+      movies: { edges: movies },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "movies", idType: URI) {
+          moviesMeta {
+            moviesPageDescription
+            moviesPageBanner {
+              altText
+              sourceUrl
+              imageFile {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+            }
+          }
+        }
+        movies {
+          edges {
+            node {
+              movie {
+                rating
+                title
+                poster {
+                  altText
+                  sourceUrl
+                  imageFile {
+                    childImageSharp {
+                      fluid(quality: 100) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
+                }
+              }
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Movies" />
+      <Wrapper moviesColor={COLORS.BLACK} descriptionColor={COLORS.SECONDARY}>
+        <div className="banner">
+          <Image
+            fluid={moviesPageBanner.imageFile.childImageSharp.fluid}
+            alt={moviesPageBanner.altText}
+          />
+          <BottomEdgeDown color={COLORS.SECONDARY} />
+        </div>
+        <div className="description">
+          <h2>Movie reviews</h2>
+          <p>{moviesPageDescription}</p>
+          <BottomEdgeUp color={COLORS.BLACK} />
+        </div>
+        <div className="movies">
+          <h2>Movies</h2>
+          <div className="movie-items">
+            {movies.map(({ node: { movie, slug } }) => (
+              <Movie to={`/${slug}`} key={slug}>
+                <Image
+                  fluid={movie.poster.imageFile.childImageSharp.fluid}
+                  alt={movie.poster.altText}
+                />
+                <div className="movie-info">
+                  <p>{movie.title}</p>
+                  <p>{movie.rating}</p>
+                </div>
+              </Movie>
+            ))}
+          </div>
+        </div>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default MoviesPage
